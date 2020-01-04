2b4d111221a5c7252ca5ce6a66d43fed566fe4d2
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-01-04 15:33:48 +0100

    .

--- src/background.js
+++ src/background.js
@@ -20,8 +20,6 @@ function createWindow () {
     nodeIntegration: true
   } })
 
-  win.maximize()
-  win.setResizable(false)
   if (process.env.WEBPACK_DEV_SERVER_URL) {
     // Load the url of the dev server if in development mode
     win.loadURL(process.env.WEBPACK_DEV_SERVER_URL)
