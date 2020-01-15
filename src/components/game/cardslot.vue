d7eeec1a338125028403f795c64a9863b3c5a095
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-01-15 23:49:14 +0100

    all function done

--- src/components/game/cardslot.vue
+++ src/components/game/cardslot.vue
@@ -1,135 +1,211 @@
 <template>
-    <div v-if="handSlot">
-        <div class="cardSlot" v-if="gameState.connectedPlayers[playerIndexData].cardsInHand[cardSlotIndexData] !== undefined" 
-        v-on:click="SelectCard(gameState.connectedPlayers[playerIndexData].cardsInHand, cardSlotIndexData)"
-        v-on:click.right="SelectCard(null, null)" > 
-             <card v-if="playerIndexData === 0" v-bind:card="gameState.connectedPlayers[playerIndexData].cardsInHand[cardSlotIndexData]"
-                v-bind:inHand="true"
-                v-bind:onField="false"/>
-                <cardback v-else/>
-        </div>
-         <div class="cardSlot" v-else> 
-        </div>
+  <div v-if="handSlot">
+    <div
+      class="cardSlot"
+      v-if="
+        gameState.connectedPlayers[playerIndexData].cardsInHand[
+          cardSlotIndexData
+        ] !== undefined
+      "
+      v-on:click="
+        SelectCard(
+          gameState.connectedPlayers[playerIndexData].cardsInHand,
+          cardSlotIndexData
+        )
+      "
+      v-on:click.right="SelectCard(null, null)"
+    >
+      <card
+        v-if="playerIndexData === 0"
+        v-bind:card="
+          gameState.connectedPlayers[playerIndexData].cardsInHand[
+            cardSlotIndexData
+          ]
+        "
+        v-bind:inHand="true"
+        v-bind:onField="false"
+      />
+      <cardback v-else />
+    </div>
+    <div class="cardSlot" v-else></div>
+  </div>
+  <div v-else>
+    <div v-if="playerIndexData === 0">
+      <div
+        class="cardSlot"
+        v-if="
+          gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[
+            cardSlotIndexData
+          ] !== undefined
+        "
+        v-on:click="
+          SelectCardOnYourField(
+            gameState.connectedPlayers[playerIndexData].cardsInHand,
+            cardSlotIndexData
+          )
+        "
+      >
+        <card
+          v-bind:card="
+            gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[
+              cardSlotIndexData
+            ].card
+          "
+          v-bind:inHand="false"
+          v-bind:onField="true"
+        />
+      </div>
+      <div
+        class="cardSlot"
+        v-else
+        v-on:click="
+          SelectEmptyYourField(
+            gameState.connectedPlayers[playerIndexData].cardsInHand,
+            cardSlotIndexData
+          )
+        "
+      ></div>
     </div>
     <div v-else>
-        <div v-if="playerIndexData === 0">
-            <div class="cardSlot" v-if="gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[cardSlotIndexData] !== undefined" 
-            v-on:click="SelectCardOnYourField(gameState.connectedPlayers[playerIndexData].cardsInHand, cardSlotIndexData)"
-            > 
-                <card v-bind:card="gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[cardSlotIndexData].card"
-                    v-bind:inHand="false"
-                    v-bind:onField="true"/>
-            </div>
-         <div class="cardSlot" v-else v-on:click="SelectEmptyYourField(gameState.connectedPlayers[playerIndexData].cardsInHand, cardSlotIndexData)"> 
-        </div>
-        </div>
-        <div v-else>
-              <div class="cardSlot" v-if="gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[cardSlotIndexData] !== undefined"
-              v-on:click="SelectTargetToAttack(gameState.connectedPlayers[playerIndexData].cardsInHand, cardSlotIndexData)"
-              v-on:click.right="SelectTargetToAttack(null, null)"> 
-                <card v-bind:card="gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[cardSlotIndexData].card"
-                    v-bind:inHand="false"
-                    v-bind:onField="true"/>
-            </div>
-            <div class="cardSlot" v-else> 
-            </div>
-        </div>
-        
+      <div
+        class="cardSlot"
+        v-if="
+          gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[
+            cardSlotIndexData
+          ] !== undefined
+        "
+        v-on:click="
+          SelectTargetToAttack(
+            gameState.connectedPlayers[playerIndexData].cardsInHand,
+            cardSlotIndexData
+          )
+        "
+        v-on:click.right="SelectTargetToAttack(null, null)"
+      >
+        <card
+          v-bind:card="
+            gameState.connectedPlayers[playerIndexData].boardrow.cardSlotList[
+              cardSlotIndexData
+            ].card
+          "
+          v-bind:inHand="false"
+          v-bind:onField="true"
+        />
+      </div>
+      <div class="cardSlot" v-else></div>
     </div>
+  </div>
 </template>
 
 <script>
-    import card from "@/components/game/card.vue"
-    import cardback from "@/components/game/cardback.vue"
-    export default {
-
-        name: "cardslot",
-        components: {
-            card,
-            cardback 
-        },
-        props:['handSlot', 'cardSlotIndex', 'playerIndex'],
-        data() {
-            return {
-                handSlotData: this.handSlot,
-                cardSlotIndexData: this.cardSlotIndex,
-                playerIndexData: this.playerIndex,
-                PlayMessage: {
-                    Subject: null,
-                    Action: null,
-                    Content: null,
-                    CardToPlay: null,
-                    SpotToPlace: null,
-                }
-            }
-        },
-            computed: {
-        gameState(){
-            return this.$store.getters.getGame
-        },
-        getSelectedCardInHand(){
-            return this.$store.getters.getSelectedCardInHand
-        },
-         getSelectedEmptyCardSlotOnYourField(){
-            return this.$store.getters.getSelectedEmptyCardSlotOnYourField
-        },
-         getSelectedCardSlotOnYourField(){
-            return this.$store.getters.getSelectedCardSlotOnYourField
-        }
+  import card from "@/components/game/card.vue";
+  import cardback from "@/components/game/cardback.vue";
+  export default {
+    name: "cardslot",
+    components: {
+      card,
+      cardback
     },
-    methods:{
-        SelectCard(cardslot, index){
-            if(cardslot !== null){
-            const data = { cardslot, index}
-            this.$store.dispatch("SelectCardInHand", data)
-            console.log(data)
-            }
-            this.$store.dispatch("SelectCardInHand", null)
+    props: ["handSlot", "cardSlotIndex", "playerIndex"],
+    data() {
+      return {
+        handSlotData: this.handSlot,
+        cardSlotIndexData: this.cardSlotIndex,
+        playerIndexData: this.playerIndex,
+        PlayMessage: {
+          Subject: null,
+          Action: null,
+          Content: null,
+          CardToPlay: null,
+          SpotToPlace: null
         },
-        SelectEmptyYourField(cardslot, index){
-            console.log("werkt wel")
-            if(this.getSelectedCardInHand !== null){
-            const data = { cardslot, index}
-            this.$store.dispatch("SelectedEmptyCardSlotOnYourField", data)
-            this.TryToPlayCard()
-            console.log(data)
-            }
-        },
-        SelectCardOnYourField(cardslot, index){
-              if(cardslot !== null){
-            const data = { cardslot, index}
-            this.$store.dispatch("SelectedCardSlotOnYourField", data)
-            console.log(data)
-            }
-            this.$store.dispatch("SelectedCardSlotOnYourField", null)
-        },
-        TryToPlayCard(){
-            this.PlayMessage.Content = this.gameState
-            this.PlayMessage.Subject = "GAME"
-            this.PlayMessage.Action = "PLACECARD"
-            this.PlayMessage.CardToPlay = this.getSelectedCardInHand
-            this.PlayMessage.SpotToPlace = this.getSelectedEmptyCardSlotOnYourField
-
-        },
-        SelectTargetToAttack(cardslot, index){
-        console.log("werkt wel")
-        if(this.SelectedCardSlotOnYourField !== null){
-        const data = { cardslot, index}
-        this.$store.dispatch("SelectCardInHand", data)
-        console.log(data)
+         AttackMessage: {
+          Subject: null,
+          Action: null,
+          Content: null,
+          CardToAttackWith: null,
+          TargetToAttack: null
         }
+      };
+    },
+    computed: {
+      gameState() {
+        return this.$store.getters.getGame;
+      },
+      getSelectedCardInHand() {
+        return this.$store.getters.getSelectedCardInHand;
+      },
+      getSelectedEmptyCardSlotOnYourField() {
+        return this.$store.getters.getSelectedEmptyCardSlotOnYourField;
+      },
+      getSelectedCardSlotOnYourField() {
+        return this.$store.getters.getSelectedCardSlotOnYourField;
+      },
+       getSelectedTargetToAttack() {
+        return this.$store.getters.getSelectedTargetToAttack;
+      }
+    },
+    methods: {
+      SelectCard(cardslot, index) {
+        if (cardslot !== null) {
+          const data = { cardslot, index };
+          this.$store.dispatch("SelectCardInHand", data);
+          console.log(data);
         }
+        this.$store.dispatch("SelectCardInHand", null);
+      },
+      SelectEmptyYourField(cardslot, index) {
+        console.log("werkt wel");
+        if (this.getSelectedCardInHand !== null) {
+          const data = { cardslot, index };
+          this.$store.dispatch("SelectedEmptyCardSlotOnYourField", data);
+          this.TryToPlayCard();
+          console.log(data);
+        }
+      },
+      SelectCardOnYourField(cardslot, index) {
+        if (cardslot !== null) {
+          const data = { cardslot, index };
+          this.$store.dispatch("SelectedCardSlotOnYourField", data);
+          console.log(data);
+          //check if card has already attacked. backend needs a boolean
+        }
+        this.$store.dispatch("SelectedCardSlotOnYourField", null);
+      },
+        SelectTargetToAttack(cardslot, index) {
+        console.log("werkt wel");
+        if (this.SelectedCardSlotOnYourField !== null) {
+          const data = { cardslot, index };
+          this.$store.dispatch("SelectedTargetToAttack", data);
+          this.AttackOpponentsCard();
+          console.log(data);
+        }
+      },
+      TryToPlayCard() {
+        this.PlayMessage.Content = this.gameState;
+        this.PlayMessage.Subject = "GAME";
+        this.PlayMessage.Action = "PLACECARD";
+        this.PlayMessage.CardToPlay = this.getSelectedCardInHand;
+        this.PlayMessage.SpotToPlace = this.getSelectedEmptyCardSlotOnYourField;
+      },
+      AttackOpponentsCard(){
+        this.AttackMessage.Content = this.gameState;
+        this.AttackMessage.Subject = "GAME";
+        this.AttackMessage.Action = "ATTACKWITHCARD";
+        this.AttackMessage.CardToAttackWith = this.getSelectedCardInHand;
+        this.AttackMessage.TargetToAttack = this.getSelectedTargetToAttack;
+      }
     }
- }
+  };
 </script>
 
 <style>
-    .cardSlot {
-        float: left;
+  .cardSlot {
+    float: left;
 
-        margin: 0 8.6px 0 8.6px;
+    margin: 0 8.6px 0 8.6px;
 
-        width: 5vw;
-        height: 12vh;
-    }
+    width: 5vw;
+    height: 12vh;
+  }
 </style>
--- src/components/game/player.vue
+++ src/components/game/player.vue
@@ -1,165 +1,213 @@
 <template>
-    <div v-bind:class="[ topPlayer ? heroContainerTop : heroContainerBottom ]">
-        <div v-bind:class="[ topPlayer ? heroHpTop : heroHpBottom ]">
-                {{ gameState.connectedPlayers[index].hero.hp }}
-            </div>
-            <div v-bind:class="[ topPlayer ? heroBlueManaTop : heroBlueManaBottom ]">
-                {{ gameState.connectedPlayers[index].hero.mana }}
-            </div>
-            <div v-bind:class="[ topPlayer ? heroDeathEssenceTop : heroDeathEssenceBottom ]">
-                {{ gameState.connectedPlayers[index].hero.deathessence }}
-            </div>
-            <div v-bind:class="[ topPlayer ? heroNameTop : heroNameBottom ]">
-                {{ gameState.connectedPlayers[index].username }}
-            </div>
+  <div
+    v-bind:class="[topPlayer ? heroContainerTop : heroContainerBottom]"
+    v-if="index === 0"
+  >
+    <div v-bind:class="[topPlayer ? heroHpTop : heroHpBottom]">
+      {{ gameState.connectedPlayers[index].hero.hp }}
     </div>
+    <div v-bind:class="[topPlayer ? heroBlueManaTop : heroBlueManaBottom]">
+      {{ gameState.connectedPlayers[index].hero.mana }}
+    </div>
+    <div
+      v-bind:class="[topPlayer ? heroDeathEssenceTop : heroDeathEssenceBottom]"
+    >
+      {{ gameState.connectedPlayers[index].hero.deathessence }}
+    </div>
+    <div v-bind:class="[topPlayer ? heroNameTop : heroNameBottom]">
+      {{ gameState.connectedPlayers[index].username }}
+    </div>
+  </div>
+  <div
+    v-bind:class="[topPlayer ? heroContainerTop : heroContainerBottom]"
+    v-else
+    v-on:click="SelectHero(gameState.connectedPlayers[index].hero)"
+  >
+    <div v-bind:class="[topPlayer ? heroHpTop : heroHpBottom]">
+      {{ gameState.connectedPlayers[index].hero.hp }}
+    </div>
+    <div v-bind:class="[topPlayer ? heroBlueManaTop : heroBlueManaBottom]">
+      {{ gameState.connectedPlayers[index].hero.mana }}
+    </div>
+    <div
+      v-bind:class="[topPlayer ? heroDeathEssenceTop : heroDeathEssenceBottom]"
+    >
+      {{ gameState.connectedPlayers[index].hero.deathessence }}
+    </div>
+    <div v-bind:class="[topPlayer ? heroNameTop : heroNameBottom]">
+      {{ gameState.connectedPlayers[index].username }}
+    </div>
+  </div>
 </template>
 
 <script>
-
-export default {
+  export default {
     name: "player",
     props: {
-        index: Number,
-        topPlayer: Boolean
+      index: Number,
+      topPlayer: Boolean
     },
     data() {
-        return {
-            heroBlueManaTop: "heroBlueManaTop",
-            heroBlueManaBottom: "heroBlueManaBottom",
-            heroDeathEssenceTop: "heroDeathEssenceTop",
-            heroDeathEssenceBottom: "heroDeathEssenceBottom",
-            heroHpTop: "heroHpTop",
-            heroHpBottom: "heroHpBottom",
-            heroNameTop: "heroNameTop",
-            heroNameBottom: "heroNameBottom",
-            heroContainerTop: "heroContainerTop",
-            heroContainerBottom: "heroContainerBottom",
-            Hero: {
-                name: "suchACoolBoy",
-                hp: 30,
-                blueMana: 10,
-                deathEssence: 10
-            }
+      return {
+        heroBlueManaTop: "heroBlueManaTop",
+        heroBlueManaBottom: "heroBlueManaBottom",
+        heroDeathEssenceTop: "heroDeathEssenceTop",
+        heroDeathEssenceBottom: "heroDeathEssenceBottom",
+        heroHpTop: "heroHpTop",
+        heroHpBottom: "heroHpBottom",
+        heroNameTop: "heroNameTop",
+        heroNameBottom: "heroNameBottom",
+        heroContainerTop: "heroContainerTop",
+        heroContainerBottom: "heroContainerBottom",
+        AttackHeroMessage: {
+          Subject: null,
+          Action: null,
+          Content: null,
+          CardToAttackWith: null,
+          Hero: null
         }
+      };
     },
     computed: {
-        gameState(){
-            return this.$store.getters.getGame
+      gameState() {
+        return this.$store.getters.getGame;
+      },
+      getSelectedCardSlotOnYourField() {
+        return this.$store.getters.getSelectedCardSlotOnYourField;
+      },
+         getSelectedHeroToAttack() {
+        return this.$store.getters.getSelectedHeroToAttack;
+      }
+    },
+    methods: {
+      SelectHero(hero) {
+        console.log("werkt wel");
+        if (this.SelectedCardSlotOnYourField !== null) {
+          const data = { cardslot, index };
+          this.$store.dispatch("SelectedHeroToAttack", data);
+          this.AttackHero();
+          console.log(data);
         }
+      },
+      AttackHero() {
+        this.AttackHeroMessage.Content = this.gameState;
+        this.AttackHeroMessage.Subject = "GAME";
+        this.AttackHeroMessage.Action = "ATTACKHERO";
+        this.AttackHeroMessage.CardToAttackWith = this.getSelectedCardSlotOnYourField;
+        this.AttackHeroMessage.Hero = this.getSelectedHeroToAttack;
+      }
     }
-
-}
+  };
 </script>
 
 <style>
-    .heroContainerTop {
-        position: absolute;
-        top: 26.5vh;
-        left: 25vw;
+  .heroContainerTop {
+    position: absolute;
+    top: 26.5vh;
+    left: 25vw;
 
-        justify-content: center;
-        display: flex;
+    justify-content: center;
+    display: flex;
 
-        padding-top: 3vh;
+    padding-top: 3vh;
 
-        width: 36vw;
-        height: 7.5vh;
-    }
+    width: 36vw;
+    height: 7.5vh;
+  }
 
-    .heroContainerBottom {
-        position: absolute;
-        bottom: 26.5vh;
-        right: 25vw;
+  .heroContainerBottom {
+    position: absolute;
+    bottom: 26.5vh;
+    right: 25vw;
 
-        justify-content: center;
-        display: flex;
+    justify-content: center;
+    display: flex;
 
-        width: 36vw;
-        height: 7.5vh;
-    }
+    width: 36vw;
+    height: 7.5vh;
+  }
 
-    .heroNameTop {
-        color: white;
+  .heroNameTop {
+    color: white;
 
-        position: absolute;
-        top: 0;
-        left: 0;
-    }
+    position: absolute;
+    top: 0;
+    left: 0;
+  }
 
-    .heroNameBottom {
-        color: white;
+  .heroNameBottom {
+    color: white;
 
-        position: absolute;
-        bottom: 0;
-        right: 0;
-    }
+    position: absolute;
+    bottom: 0;
+    right: 0;
+  }
 
-    .heroHpTop {
-        color: white;
+  .heroHpTop {
+    color: white;
 
-        position: absolute;
-        bottom: 0;
-        right: 10.2vw;
+    position: absolute;
+    bottom: 0;
+    right: 10.2vw;
 
-        width: 25px;
-        height: 25px;
-    }
+    width: 25px;
+    height: 25px;
+  }
 
-    .heroHpBottom {
-        color: white;
+  .heroHpBottom {
+    color: white;
 
-        position: absolute;
-        top: 0;
-        left: 10.2vw;
+    position: absolute;
+    top: 0;
+    left: 10.2vw;
 
-        margin-top: -.6vh;
+    margin-top: -0.6vh;
 
-        width: 25px;
-        height: 25px;
-    }
+    width: 25px;
+    height: 25px;
+  }
 
-    .heroBlueManaTop {
-        color: white;
+  .heroBlueManaTop {
+    color: white;
 
-        position: absolute;
-        left: 16vw;
-        top: 2vh;
+    position: absolute;
+    left: 16vw;
+    top: 2vh;
 
-        width: 25px;
-        height: 25px;
-    }
+    width: 25px;
+    height: 25px;
+  }
 
-    .heroBlueManaBottom {
-        color: white;
+  .heroBlueManaBottom {
+    color: white;
 
-        position: absolute;
-        right: 16vw;
-        bottom: 2vh;
+    position: absolute;
+    right: 16vw;
+    bottom: 2vh;
 
-        width: 25px;
-        height: 25px;
-    }
+    width: 25px;
+    height: 25px;
+  }
 
-    .heroDeathEssenceTop {
-        color: white;
+  .heroDeathEssenceTop {
+    color: white;
 
-        position: absolute;
-        right: 2.4vw;
-        top: 1.5vh;
+    position: absolute;
+    right: 2.4vw;
+    top: 1.5vh;
 
-        width: 25px;
-        height: 25px;
-    }
+    width: 25px;
+    height: 25px;
+  }
 
-    .heroDeathEssenceBottom {
-        color: white;
+  .heroDeathEssenceBottom {
+    color: white;
 
-        position: absolute;
-        left: 2.3vw;
-        bottom: 1.8vh;
+    position: absolute;
+    left: 2.3vw;
+    bottom: 1.8vh;
 
-        width: 25px;
-        height: 25px;
-    }
+    width: 25px;
+    height: 25px;
+  }
 </style>
