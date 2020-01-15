aac94ffe1e7e82763c1196cf71afaa9838c0ce1d
MarioHendriks <43466121+MarioHendriks@users.noreply.github.com>
2020-01-15 15:47:20 +0100

    Update endButton.vue

--- src/components/game/endButton.vue
+++ src/components/game/endButton.vue
@@ -1,22 +1,47 @@
 <template>
-    <div class="endButton artButton">
-        End turn
-    </div>
+  <div class="endButton artButton" v-on:click="endTurn">
+    End turn
+  </div>
 </template>
 
 <script>
-export default {
-
-}
+  export default {
+    name: "endButton",
+    data() {
+      return {
+        id: "",
+        wsMessage: {
+          Subject: null,
+          Action: null,
+          Content: null,
+          Token: null
+        }
+      };
+    },
+    computed: {
+      gameState() {
+        return this.$store.getters.getGame;
+      }
+    },
+    methods: {
+      async endTurn() {
+        this.wsMessage.Subject = "GAME";
+        this.wsMessage.Action = "ENDTURN";
+        this.wsMessage.Content = this.gameState;
+        this.wsMessage.Token = await this.$auth.getTokenSilently();
+        this.$socket.send(JSON.stringify(this.wsMessage));
+      }
+    }
+  };
 </script>
 
 <style>
-    .endButton {
-        margin: auto;
-        padding-top: 9px;
+  .endButton {
+    margin: auto;
+    padding-top: 9px;
 
-        position: absolute;
-        right: 2.5vw;
-        bottom: 48vh;
-    }
+    position: absolute;
+    right: 2.5vw;
+    bottom: 48vh;
+  }
 </style>
