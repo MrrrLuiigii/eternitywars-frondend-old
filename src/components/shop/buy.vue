f7765c4a2bafc652fe4dd16b85e5f21c51e2c90d
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-01-18 17:33:20 +0100

    style shop

--- src/components/shop/buy.vue
+++ src/components/shop/buy.vue
@@ -1,62 +1,121 @@
 <template>
-  <div>
-      <div> 
-      <div v-on:click="Increase">PLUS</div>
-      <div>{{amount}}</div>
-      <div v-on:click="Decrease" >MINUS</div>
-      <div v-on:click="BuyPack">BUY PACK NOW</div> 
-      <div>BUY WITH PAYPAL</div> 
+  <div class="buyContainer">
+    <div class="buyAmountContainer">
+      <div class="packButtonMinContainer">
+        <button class="artSButton" v-on:click="decrease">
+          -
+        </button>
       </div>
+      <div class="packAmount">{{ amount }}</div>
+      <div class="packButtonPlusContainer">
+        <button class="artSButton" v-on:click="increase">
+          +
+        </button>
+      </div>
+    </div>
+    <div>
+      <button class="artXButton" v-on:click="buyPack">Buy with gold</button>
+      <button class="artXButton">Buy with PayPal</button>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
-    data(){
-        return{
-            amount: 1,
-             wsMessage: {
-              Subject: null,
-              Action: null,
-              Content: null,
-              Amount: null,
-              Token: null
-        },
-        }
-    },
-    computed:{
-        getPlayerInfo(){
-          return this.$store.getters.getPlayerInfo;
-        }
-    },
-    methods:{
-        Increase(){
-            this.amount = this.amount+1
-        },
-        Decrease(){
-            if(this.amount !==  1){
-                this.amount = this.amount-1
-            }
-            this.$toasted.show("can't go any lower.", {
-            theme: "toasted-primary",
-            position: "bottom-right",
-            duration: 2500
-          });
-        },
-        async BuyPack(){
-            this.wsMessage.Subject = "SHOP"
-            this.wsMessage.Action = "BUYPACK"
-            this.wsMessage.Content = this.getPlayerInfo
-            this.wsMessage.Token = await this.$auth.getTokenSilently()
-            this.wsMessage.Amount = this.amount
-            this.$socket.send(JSON.stringify(this.wsMessage))
-            console.log(this.wsMessage)
-            this.amount = 1
-        }
+  data() {
+    return {
+      amount: 1,
+      wsMessage: {
+        Subject: null,
+        Action: null,
+        Content: null,
+        Amount: null,
+        Token: null
+      }
+    };
+  },
+  computed: {
+    getPlayerInfo() {
+      return this.$store.getters.getPlayerInfo;
     }
-}
+  },
+  methods: {
+    increase() {
+      this.amount = this.amount + 1;
+    },
+    decrease() {
+      if (this.amount > 1) {
+        this.amount = this.amount - 1;
+      } else {
+        this.showToast("You can't buy 0 or less packs, you fool...");
+      }
+    },
+    async buyPack() {
+      this.wsMessage.Subject = "SHOP";
+      this.wsMessage.Action = "BUYPACK";
+      this.wsMessage.Content = this.getPlayerInfo;
+      this.wsMessage.Token = await this.$auth.getTokenSilently();
+      this.wsMessage.Amount = this.amount;
+      this.$socket.send(JSON.stringify(this.wsMessage));
+      this.amount = 1;
+    },
+    async showToast(message) {
+      this.$toasted.show(message, {
+        theme: "toasted-primary",
+        position: "bottom-right",
+        duration: 2500
+      });
+    }
+  }
+};
 </script>
 
 <style>
+.buyContainer {
+  height: 60vh;
+  width: 50vw;
 
+  justify-content: center;
+}
+
+.buyAmountContainer {
+  width: 30vw;
+
+  padding-top: 1vh;
+
+  display: flex;
+
+  border: 20px;
+  border-color: #362630;
+  border-style: groove ridge ridge groove;
+
+  background-color: #a5b495;
+
+  margin: auto;
+  margin-bottom: 5vh;
+}
+
+.packButtonMinContainer {
+  width: 10vw;
+  height: 10vh;
+
+  padding-top: 2.5vh;
+
+  float: left;
+}
+
+.packButtonPlusContainer {
+  width: 10vw;
+  height: 10vh;
+
+  padding-top: 2.5vh;
+
+  float: right;
+}
+
+.packAmount {
+  font-size: 60px;
+  width: 10vw;
+  height: 10vh;
+}
 </style>
--- src/components/shop/open.vue
+++ src/components/shop/open.vue
@@ -1,62 +1,71 @@
 <template>
-  <div>
-        <div v-if="this.getPlayerInfo.packAmount !== null"> 
-      <div> 
-            <div v-if="this.getPack !== null">
-                <div v-for="(card, index) in this.getPack.Cards"
-                :key="index"
-                :card="card">
-                    <card :card="card"/>
-                </div>
-            </div>
+  <div class="packOpenContainer" v-if="this.getPlayerInfo.packAmount !== null">
+    <div class="openedCardsContainer" v-if="this.getPack !== null">
+      <div
+        v-for="(card, index) in this.getPack.cards"
+        :key="index"
+        :card="card"
+      >
+        <card :card="card" />
       </div>
-      <div>{{getPlayerInfo.packAmount}}</div>
-      <div v-on:click="openPack">OPEN</div> 
-      </div>
-       <div v-else> 
-      <div > You dont have any packs to open lets buy some </div>
-      </div>
+    </div>
+    <button class="artXButton" v-on:click="openPack">Open pack</button>
+  </div>
+  <div class="packOpenContainer" v-else>
+    <div>You dont have any packs to open lets buy some</div>
   </div>
 </template>
 
 <script>
-import card from "@/components/game/card"
+import card from "@/components/game/card";
 export default {
-    components:{
-        card
-    },
- data(){
-        return{
-            amount: 1,
-             wsMessage: {
-              Subject: null,
-              Action: null,
-              Content: null,
-              Token: null
-        },
-    } 
-},
-  computed:{
-    getPack(){
-      return this.$store.getters.getPack
-    },
-    getPlayerInfo(){
-     return this.$store.getters.getPlayerInfo;
-     },
+  components: {
+    card
   },
-  methods:{
-      async openPack(){
-        this.wsMessage.Subject = "SHOP"
-        this.wsMessage.Action = "OPENPACK"
-        this.wsMessage.Content = this.getPlayerInfo
-        this.wsMessage.Token = await this.$auth.getTokenSilently()
-        this.$socket.send(JSON.stringify(this.wsMessage))
-        console.log(this.wsMessage)
+  data() {
+    return {
+      amount: 1,
+      wsMessage: {
+        Subject: null,
+        Action: null,
+        Content: null,
+        Token: null
       }
+    };
+  },
+  computed: {
+    getPack() {
+      return this.$store.getters.getPack;
+    },
+    getPlayerInfo() {
+      return this.$store.getters.getPlayerInfo;
+    }
+  },
+  methods: {
+    async openPack() {
+      console.log("yeet");
+      console.log(this.getPack);
+
+      this.wsMessage.Subject = "SHOP";
+      this.wsMessage.Action = "OPENPACK";
+      this.wsMessage.Content = this.getPlayerInfo;
+      this.wsMessage.Token = await this.$auth.getTokenSilently();
+      this.$socket.send(JSON.stringify(this.wsMessage));
+    }
   }
-}
+};
 </script>
 
 <style>
+.packOpenContainer {
+  height: 100vh;
+  width: 50vw;
+  background-color: red;
+}
 
+.openedCardsContainer {
+  height: 60vh;
+  width: 100%;
+  background-color: green;
+}
 </style>
