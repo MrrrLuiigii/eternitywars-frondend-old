ba0c8135cf5398791b3e18c79b953bf520e2c15b
MrrrLuiigii <43231781+MrrrLuiigii@users.noreply.github.com>
2020-01-18 18:20:31 +0100

    format

--- src/store/shopModule.js
+++ src/store/shopModule.js
@@ -1,20 +1,20 @@
 export default {
-    state: {
-        pack: null,
-    },
-    getters: {
-        getPack: state =>{
-            return state.pack;
-        }
-    },
-    mutations: {
-     ADD_PACK(state, pack){
-         state.pack = pack
-     }
-    },
-    actions: {
-        AddPack({commit}, pack){
-            commit("ADD_PACK", pack)
-        }
+  state: {
+    pack: null
+  },
+  getters: {
+    getPack: state => {
+      return state.pack;
     }
-}
+  },
+  mutations: {
+    ADD_PACK(state, pack) {
+      state.pack = pack;
+    }
+  },
+  actions: {
+    AddPack({ commit }, pack) {
+      commit("ADD_PACK", pack);
+    }
+  }
+};
